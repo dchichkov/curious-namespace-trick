3edbdca24341aa2a26dfcb2ee6f3aff19b3f189f
Dmitry Chichkov <dchichkov@gmail.com>
2016-01-20 12:08:34 -0800

    Add test code for templates.

---
+++ templ.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+// output:
+//  dmitry@t:~$ g++ -std=c++11 templ.cpp -Dplatform= ; ./a.out 
+//  common add
+//  dmitry@t:~$ g++ -std=c++11 templ.cpp -Dplatform=arm ; ./a.out 
+//  arm add
+
+namespace project {
+  // arm/math.h
+  namespace arm {
+    template<typename T> inline void add_() {printf("arm T add\n");}
+  }
+
+  // math.h
+  template<typename T> inline void add_() {
+    // wonderfull code
+    printf("common T add\n");
+    // mode code
+    // even more code
+  } inline namespace platform {template<typename T>  inline void add() {add_<T>();}}
+
+
+  template<typename T> inline void dot_() {
+    // wonderfull code
+    add<T>();
+    // this dot template function is a long one
+
+  } inline namespace platform {template<typename T> inline void dot() {dot_<T>();}}
+}
+
+int main() {
+ project::dot<int>();
+ return 1;
+}
