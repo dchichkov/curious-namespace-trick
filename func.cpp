1903b9b9779d1f93c5ef0bb5cb8611c24678efdf
Dmitry Chichkov <dchichkov@gmail.com>
2016-01-20 12:05:03 -0800

    Add test code fot functions.

---
+++ func.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// output:
+//  dmitry@t:~$ g++ -std=c++11 func.cpp -Dplatform=common ; ./a.out 
+//  common add
+//  dmitry@t:~$ g++ -std=c++11 func.cpp -Dplatform=arm ; ./a.out 
+//  arm add
+
+namespace project {
+  // arm/math.h
+  namespace arm {
+    inline void add_() {printf("arm add\n");  // try comment out
+  }
+
+  // math.h
+  inline void add_() {
+    //
+    printf("common add\n");
+    //
+  } inline namespace platform {inline void add() {add_();}}
+
+
+  inline void dot_() {
+    //
+    add();
+    //
+  } inline namespace platform {inline void dot() {dot_();}}
+}
+
+int main() {
+ project::dot();
+ return 1;
+}
